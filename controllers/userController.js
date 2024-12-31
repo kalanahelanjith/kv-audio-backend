a6961de025f73165789a530f6295e38c8f24d24d
Kalana Helanjith <kalanahelanjith@gmail.com>
2024-12-31 11:55:53 +0530

    Initialized the backend repo and create authentication

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken" ;
+
+export function registerUser(req, res){
+
+    const data = req.body;
+
+    data.password = bcrypt.hashSync(data.password,10)
+
+    const newUser = new User(data)
+    
+
+    newUser.save().then(() => {
+        res.json({message: "User registered sussessfully"})
+    }).catch((error) => {
+        res.status(500).json({error : "User registered faield"})
+    })
+}
+
+export function loginUser(req, res){
+    const data = req.body;
+
+    User.findOne({
+        email: data.email
+    }).then(
+        (user) => {
+            if (user == null) {
+                res.status(404).json({ error: "User not found"});
+            } else {
+                
+
+                const isPasswordCorrect = bcrypt.compareSync(data.password, user.password);
+
+                if (isPasswordCorrect) {
+
+                    const token = jwt.sign({
+                        firstNmae: user.firstNmae,
+                        lastNmae : user.lastName,
+                        email : user.email,
+                        role: user.role
+
+                    },"kv-secret-89!")
+
+
+                    res.json({ message: "Login successful",token : token});
+                } else {
+                    res.status(401).json({error: "Login faild"});
+                }
+
+                
+            }
+        }
+    );
+
+}
---
+++ routes/userRouter.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { registerUser } from "../controllers/userController.js";
+import { loginUser } from "../controllers/userController.js";
+
+
+const userRouter = express.Router();
+
+userRouter.post("/", registerUser)
+
+userRouter.post("/login",loginUser)
+
+export default userRouter;
