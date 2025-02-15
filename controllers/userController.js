2df770ea78b0717bd1a2fd069a2a9a372120adea
Kalana Helanjith <kalanahelanjith@gmail.com>
2025-02-16 00:08:06 +0530

    user login function implementation 😍

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,7 +48,7 @@ export function loginUser(req, res){
                     },process.env.JWT_SECRET);
 
 
-                    res.json({ message: "Login successful",token : token});
+                    res.json({ message: "Login successful",token : token, user:user});
                 } else {
                     res.status(401).json({error: "Login faild"});
                 }
